b741db3fd23aa2616965273cf7f4713a53374d8f
Saturday Academy <satacad@Banana1.local>
2013-04-27 11:04:26 -0700

    fistdeploye

--- app.rb
+++ app.rb
@@ -2,14 +2,48 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  erb :index   
+  erb :index
 end
 
+get '/login' do
+  @username = params[:username]
+  @password = params[:password]
+  if @username == 'Lucas'
+    if @password == 'welcome'
+      redirect "/name?name=Lucas&state=Oregon"
+    else
+      @error = "Wrong Password"
+      erb :index
+    end
+  else
+    @error = "Wrong username"
+    erb :index
+  end
+end
+
+post "/login" do
+  @username = params[:username]
+  @password = params[:password]
+  if @username== 'Lucas'
+    if @password == 'welcome'
+      erb :home 
+    else
+      @error = "stupid idiot"
+      erb :index
+    end 
+  else
+    @error = "thats not the right name"
+  end
+end
+      
+      
 get '/name' do
-  @name = (params[:name] || "Nobody").downcase
+  @name = (params[:name] || "Nobody")
+  @state = params[:state]
   if @name == "collin"
     redirect 'http://www.youtube.com/watch?v=oHg5SJYRHA0'
   else
     erb :name
   end
 end
+
